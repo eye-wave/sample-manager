a08da7655cd53be990131f08f88a58eef6951133
eyewave <eyewave@noreply.codeberg.org>
2026-06-02 01:40:36 +0200

    build fix

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,12 +1,14 @@
 import cssNano from "cssnano";
-import { defineConfig } from "vite";
-import injectHTML from "vite-plugin-html-inject";
-import { viteSingleFile } from "vite-plugin-singlefile";
 import htmlTemplateMinifyPlugin from "./src-build/minify-templates";
+import injectHTML from "vite-plugin-html-inject";
 import { betterEnums } from "vite-plugin-better-enums";
+import { defineConfig } from "vite";
+import { viteSingleFile } from "vite-plugin-singlefile";
+
+const isProd = import.meta.env?.PROD ?? process.env.NODE_ENV === "production";
 
 export default defineConfig({
-  publicDir: "../assets",
+  publicDir: isProd ? "public" : "../assets",
   css: {
     postcss: {
       plugins: [cssNano({ preset: "advanced" })],
